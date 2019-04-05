ed52ec78d99a09f820cf14a0de7be20a3272e7ee
PiotrKedra <piotr.k9750@gmail.com>
2019-04-05 11:53:39 +0200

    final SOAP

--- helloA-connector/src/main/java/consumption/MainTest.java
+++ helloA-connector/src/main/java/consumption/MainTest.java
@@ -1,16 +1,17 @@
 package consumption;
 
-import helloG.HelloResponse;
-import helloG.Student;
-import helloG.StudentService;
-import helloG.StudentServiceService;
+import helloG.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.xml.ws.BindingProvider;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainTest {
 
@@ -51,5 +52,25 @@ public class MainTest {
         frame.getContentPane().add(new JLabel(new ImageIcon(imgB)));
         frame.pack();
         frame.setVisible(true);
+
+
+        StudentManageService studentManageService = new StudentManageService();
+
+
+
+        StudentManage studentManage = studentManageService.getStudentManagePort();
+
+        setCredentials((BindingProvider) studentManage,"manager", "manager1");
+
+
+        studentManage.addStudent("51", "Ziomek", "Mat", "wp@wp.pl", Arrays.asList("KOR", "WOR"));
+
+
+        System.out.println(studentService.getStudentById("51").getFirstName() + studentService.getStudentById("51").getLastName());
+    }
+
+    static private void setCredentials(BindingProvider provider, String username, String password){
+        provider.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, username);
+        provider.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
     }
 }
--- helloA-connector/src/main/java/helloG/ObjectFactory.java
+++ helloA-connector/src/main/java/helloG/ObjectFactory.java
@@ -10,7 +10,7 @@ import javax.xml.namespace.QName;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the helloG package.
+ * generated in the hellog package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -24,262 +24,100 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _AghEduPlSoaStudent_QNAME = new QName("http://helloG/", "agh.edu.pl.soa.Student");
-    private final static QName _FiltrStudents_QNAME = new QName("http://helloG/", "filtr_students");
-    private final static QName _FiltrStudentsResponse_QNAME = new QName("http://helloG/", "filtr_studentsResponse");
-    private final static QName _GetAllStudents_QNAME = new QName("http://helloG/", "getAllStudents");
-    private final static QName _GetAllStudentsResponse_QNAME = new QName("http://helloG/", "getAllStudentsResponse");
-    private final static QName _GetAvatar_QNAME = new QName("http://helloG/", "getAvatar");
-    private final static QName _GetAvatarResponse_QNAME = new QName("http://helloG/", "getAvatarResponse");
-    private final static QName _GetStudentById_QNAME = new QName("http://helloG/", "getStudentById");
-    private final static QName _GetStudentByIdResponse_QNAME = new QName("http://helloG/", "getStudentByIdResponse");
-    private final static QName _Hello_QNAME = new QName("http://helloG/", "hello");
-    private final static QName _HelloResponse_QNAME = new QName("http://helloG/", "helloResponse");
+    private final static QName _AddStudent_QNAME = new QName("http://helloG/", "addStudent");
+    private final static QName _AddStudentResponse_QNAME = new QName("http://helloG/", "addStudentResponse");
+    private final static QName _EditStudent_QNAME = new QName("http://helloG/", "editStudent");
+    private final static QName _EditStudentResponse_QNAME = new QName("http://helloG/", "editStudentResponse");
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: helloG
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: hellog
      * 
      */
     public ObjectFactory() {
     }
 
     /**
-     * Create an instance of {@link Student }
+     * Create an instance of {@link AddStudent }
      * 
      */
-    public Student createStudent() {
-        return new Student();
+    public AddStudent createAddStudent() {
+        return new AddStudent();
     }
 
     /**
-     * Create an instance of {@link FiltrStudents }
+     * Create an instance of {@link AddStudentResponse }
      * 
      */
-    public FiltrStudents createFiltrStudents() {
-        return new FiltrStudents();
+    public AddStudentResponse createAddStudentResponse() {
+        return new AddStudentResponse();
     }
 
     /**
-     * Create an instance of {@link FiltrStudentsResponse }
+     * Create an instance of {@link EditStudent }
      * 
      */
-    public FiltrStudentsResponse createFiltrStudentsResponse() {
-        return new FiltrStudentsResponse();
+    public EditStudent createEditStudent() {
+        return new EditStudent();
     }
 
     /**
-     * Create an instance of {@link GetAllStudents }
+     * Create an instance of {@link EditStudentResponse }
      * 
      */
-    public GetAllStudents createGetAllStudents() {
-        return new GetAllStudents();
+    public EditStudentResponse createEditStudentResponse() {
+        return new EditStudentResponse();
     }
 
     /**
-     * Create an instance of {@link GetAllStudentsResponse }
-     * 
-     */
-    public GetAllStudentsResponse createGetAllStudentsResponse() {
-        return new GetAllStudentsResponse();
-    }
-
-    /**
-     * Create an instance of {@link GetAvatar }
-     * 
-     */
-    public GetAvatar createGetAvatar() {
-        return new GetAvatar();
-    }
-
-    /**
-     * Create an instance of {@link GetAvatarResponse }
-     * 
-     */
-    public GetAvatarResponse createGetAvatarResponse() {
-        return new GetAvatarResponse();
-    }
-
-    /**
-     * Create an instance of {@link GetStudentById }
-     * 
-     */
-    public GetStudentById createGetStudentById() {
-        return new GetStudentById();
-    }
-
-    /**
-     * Create an instance of {@link GetStudentByIdResponse }
-     * 
-     */
-    public GetStudentByIdResponse createGetStudentByIdResponse() {
-        return new GetStudentByIdResponse();
-    }
-
-    /**
-     * Create an instance of {@link Hello }
-     * 
-     */
-    public Hello createHello() {
-        return new Hello();
-    }
-
-    /**
-     * Create an instance of {@link HelloResponse }
-     * 
-     */
-    public HelloResponse createHelloResponse() {
-        return new HelloResponse();
-    }
-
-    /**
-     * Create an instance of {@link Student.Courses }
-     * 
-     */
-    public Student.Courses createStudentCourses() {
-        return new Student.Courses();
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Student }{@code >}
+     * Create an instance of {@link JAXBElement }{@code <}{@link AddStudent }{@code >}
      * 
      * @param value
      *     Java instance representing xml element's value.
      * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link Student }{@code >}
+     *     the new instance of {@link JAXBElement }{@code <}{@link AddStudent }{@code >}
      */
-    @XmlElementDecl(namespace = "http://helloG/", name = "agh.edu.pl.soa.Student")
-    public JAXBElement<Student> createAghEduPlSoaStudent(Student value) {
-        return new JAXBElement<Student>(_AghEduPlSoaStudent_QNAME, Student.class, null, value);
+    @XmlElementDecl(namespace = "http://helloG/", name = "addStudent")
+    public JAXBElement<AddStudent> createAddStudent(AddStudent value) {
+        return new JAXBElement<AddStudent>(_AddStudent_QNAME, AddStudent.class, null, value);
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link FiltrStudents }{@code >}
+     * Create an instance of {@link JAXBElement }{@code <}{@link AddStudentResponse }{@code >}
      * 
      * @param value
      *     Java instance representing xml element's value.
      * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link FiltrStudents }{@code >}
+     *     the new instance of {@link JAXBElement }{@code <}{@link AddStudentResponse }{@code >}
      */
-    @XmlElementDecl(namespace = "http://helloG/", name = "filtr_students")
-    public JAXBElement<FiltrStudents> createFiltrStudents(FiltrStudents value) {
-        return new JAXBElement<FiltrStudents>(_FiltrStudents_QNAME, FiltrStudents.class, null, value);
+    @XmlElementDecl(namespace = "http://helloG/", name = "addStudentResponse")
+    public JAXBElement<AddStudentResponse> createAddStudentResponse(AddStudentResponse value) {
+        return new JAXBElement<AddStudentResponse>(_AddStudentResponse_QNAME, AddStudentResponse.class, null, value);
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link FiltrStudentsResponse }{@code >}
+     * Create an instance of {@link JAXBElement }{@code <}{@link EditStudent }{@code >}
      * 
      * @param value
      *     Java instance representing xml element's value.
      * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link FiltrStudentsResponse }{@code >}
+     *     the new instance of {@link JAXBElement }{@code <}{@link EditStudent }{@code >}
      */
-    @XmlElementDecl(namespace = "http://helloG/", name = "filtr_studentsResponse")
-    public JAXBElement<FiltrStudentsResponse> createFiltrStudentsResponse(FiltrStudentsResponse value) {
-        return new JAXBElement<FiltrStudentsResponse>(_FiltrStudentsResponse_QNAME, FiltrStudentsResponse.class, null, value);
+    @XmlElementDecl(namespace = "http://helloG/", name = "editStudent")
+    public JAXBElement<EditStudent> createEditStudent(EditStudent value) {
+        return new JAXBElement<EditStudent>(_EditStudent_QNAME, EditStudent.class, null, value);
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetAllStudents }{@code >}
+     * Create an instance of {@link JAXBElement }{@code <}{@link EditStudentResponse }{@code >}
      * 
      * @param value
      *     Java instance representing xml element's value.
      * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetAllStudents }{@code >}
+     *     the new instance of {@link JAXBElement }{@code <}{@link EditStudentResponse }{@code >}
      */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getAllStudents")
-    public JAXBElement<GetAllStudents> createGetAllStudents(GetAllStudents value) {
-        return new JAXBElement<GetAllStudents>(_GetAllStudents_QNAME, GetAllStudents.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetAllStudentsResponse }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetAllStudentsResponse }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getAllStudentsResponse")
-    public JAXBElement<GetAllStudentsResponse> createGetAllStudentsResponse(GetAllStudentsResponse value) {
-        return new JAXBElement<GetAllStudentsResponse>(_GetAllStudentsResponse_QNAME, GetAllStudentsResponse.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetAvatar }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetAvatar }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getAvatar")
-    public JAXBElement<GetAvatar> createGetAvatar(GetAvatar value) {
-        return new JAXBElement<GetAvatar>(_GetAvatar_QNAME, GetAvatar.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetAvatarResponse }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetAvatarResponse }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getAvatarResponse")
-    public JAXBElement<GetAvatarResponse> createGetAvatarResponse(GetAvatarResponse value) {
-        return new JAXBElement<GetAvatarResponse>(_GetAvatarResponse_QNAME, GetAvatarResponse.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetStudentById }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetStudentById }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getStudentById")
-    public JAXBElement<GetStudentById> createGetStudentById(GetStudentById value) {
-        return new JAXBElement<GetStudentById>(_GetStudentById_QNAME, GetStudentById.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link GetStudentByIdResponse }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link GetStudentByIdResponse }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "getStudentByIdResponse")
-    public JAXBElement<GetStudentByIdResponse> createGetStudentByIdResponse(GetStudentByIdResponse value) {
-        return new JAXBElement<GetStudentByIdResponse>(_GetStudentByIdResponse_QNAME, GetStudentByIdResponse.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Hello }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link Hello }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "hello")
-    public JAXBElement<Hello> createHello(Hello value) {
-        return new JAXBElement<Hello>(_Hello_QNAME, Hello.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link HelloResponse }{@code >}
-     * 
-     * @param value
-     *     Java instance representing xml element's value.
-     * @return
-     *     the new instance of {@link JAXBElement }{@code <}{@link HelloResponse }{@code >}
-     */
-    @XmlElementDecl(namespace = "http://helloG/", name = "helloResponse")
-    public JAXBElement<HelloResponse> createHelloResponse(HelloResponse value) {
-        return new JAXBElement<HelloResponse>(_HelloResponse_QNAME, HelloResponse.class, null, value);
+    @XmlElementDecl(namespace = "http://helloG/", name = "editStudentResponse")
+    public JAXBElement<EditStudentResponse> createEditStudentResponse(EditStudentResponse value) {
+        return new JAXBElement<EditStudentResponse>(_EditStudentResponse_QNAME, EditStudentResponse.class, null, value);
     }
 
 }
